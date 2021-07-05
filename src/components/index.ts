40d5f0bf506f99069c5bc268aa5e4a157685e5f3
David Fariña <info@davidfarina.com>
2021-07-05 17:21:00 +0200

    fixes, add exports

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export * from "./FieldBase";
+export * from "./FieldCheckbox";
+export * from "./FieldCheckboxGroup";
+export * from "./FieldRadioGroup";
+export * from "./FieldText";
+export * from "./FieldSelect";
+export * from "./FieldDate";
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from "./components";
+export * from "./validators";
+export * from "./hooks";
