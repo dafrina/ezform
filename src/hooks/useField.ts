aeecc44746a1d078444155bb1903e2ea1ed6ad5a
David Fariña <info@davidfarina.com>
2022-07-12 16:20:43 +0200

    fix setting false as defaultValue

--- src/hooks/useField.ts
+++ src/hooks/useField.ts
@@ -2,18 +2,20 @@ import { useEffect } from "react";
 import { FormRefObject } from "./useForm";
 
 export const useField = (name: string, validator: (value: string) => string | null, form: FormRefObject, defaultValue?: any) => {
+	const hasDefaultValue = defaultValue !== null && typeof defaultValue !== "undefined";
+
 	useEffect(() => {
 		form.setValidators((prev) => ({ ...prev, [name]: validator }));
 		form.setMounted((prev) => ({ ...prev, [name]: true }));
 
-		if (defaultValue) form.setField(name, defaultValue, false);
+		if (hasDefaultValue) form.setField(name, defaultValue, false);
 
 		return () => {
 			form.setValidators((prev) => ({ ...prev, [name]: null }));
 			form.setErrors((prev) => ({ ...prev, [name]: null }));
 			form.setMounted((prev) => ({ ...prev, [name]: false }));
 
-			if (defaultValue) form.setField(name, null, false);
+			if (hasDefaultValue) form.setField(name, null, false);
 		};
 	}, []);
 };
